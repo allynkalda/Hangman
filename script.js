710d5c1392d4140dec583f1488485fed95cdf37e
Allyn <44602686+allynkalda@users.noreply.github.com>
2019-03-27 00:44:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Create random word generator
 var wordArray = [];
-var randomWords = [ "Awkward", "Bagpipes", "Banjo", "Bungler", "Croquet", "Crypt", "Dwarves", "Fervid", "Fishhook", "Fjord", "Gazebo", "Gypsy", "Haiku", "Haphazard", "Hyphen", "Ivory", "Jazzy", "Jiffy", "Jinx", "Jukebox", "Kayak", "Memento", "Oxygen", "Pajama", "Pixel", "Zombie", "Tomato", "Shark", "Elephant", "Island",]
+var randomWords = [ "awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "Dwarves", "fervid", "fishhook", "fjord", "gazebo", "gypsy", "haiku", "haphazard", "hyphen", "Ivory", "jazzy", "jiffy", "jinx", "jukebox", "kayak", "memento", "oxygen", "pajama", "Pixel", "zombie", "tomato", "shark", "elephant", "island",]
+
+var number = Math.floor((Math.random() * 29) + 1);
+var word = randomWords[number];
 
 // Start function picks random word
 
 function start() {
-	var number = Math.floor((Math.random() * 29) + 1);
-	var word = randomWords[number];
 	length();
 
 // Calculates length of word
@@ -19,25 +20,37 @@ function start() {
 		// Push each letter into an array
 		for (var i = 0; i < word.length; i++) {
 			wordArray.push(word[i]);
+		// Visualize the letters of the words
+			var parent = document.querySelector("ul")
+			var h = document.createElement("li");               
+			var t = document.createTextNode("__"); 
+			h.appendChild(t);
+			document.body.insertBefore(h, button);
 		}
 		console.log(wordArray);
 	}
+
+}
+
+
 // Receives letter input from player
-	function guess(input) {
+function guess(input) {
+	var input;
+	//= document.getElementsByClassName("letter");
 		// Loop if input letter is same as letters in array
 		for (var j = 0; j < word.length; j++) {
 			if (input == word[j]) {
+				// Change appended list to show letter
+				var modified = document.createElement("li");
+				modified.textContent = word[j];
+				parent.replaceChild(modified, parent.children[j]);
 				console.log("yep");
 			} else {
 				console.log("nope");
 			}
 		}
-		
-	}
-
- guess("e");
+	console.log(input);
 }
-
 
 
 
