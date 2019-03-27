4a4a929d919112e90745bd61c430b15374049831
Allyn <44602686+allynkalda@users.noreply.github.com>
2019-03-27 20:38:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,54 +4,104 @@ var randomWords = [ "awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt
 
 var number = Math.floor((Math.random() * 29) + 1);
 var word = randomWords[number];
-
 // Start function picks random word
 
 function start() {
 	length();
-
+// Hides start button once clicked
+	document.getElementById("button").style.visibility = "hidden";
 // Calculates length of word
 
 	function length() { 
 		var printLetter = word.length;
 		// Prints out number of letters
 		console.log(printLetter);
-
+		var parent = document.querySelector("ul");
 		// Push each letter into an array
 		for (var i = 0; i < word.length; i++) {
 			wordArray.push(word[i]);
-		// Visualize the letters of the words
-			var parent = document.querySelector("ul")
-			var h = document.createElement("li");               
-			var t = document.createTextNode("__"); 
+		// Visualize the blanks of the letters
+			var h = document.createElement("li");
+			h.setAttribute("id", i);              
+			var t = document.createTextNode("_"); 
 			h.appendChild(t);
-			document.body.insertBefore(h, button);
+			document.body.insertBefore(h, parent);
 		}
 		console.log(wordArray);
 	}
 
+
 }
-// Counts the times guess() is called
-var counter = 0;
+
 
 // Receives letter input from player
+var counter = 0;
+var lettersGuessed = 0;
+var photo = 0;
+
 function guess(input) {
 	counter++;
 	var input;
-	//= document.getElementsByClassName("letter");
+	var hangs = 0;
+	
+	
+	// Print number of guesses in counter var
+		document.getElementById("numOfGuesses").innerHTML = "Guess Count: " + counter;
+
 		// Loop if input letter is same as letters in array
 		for (var j = 0; j < word.length; j++) {
 			if (input == word[j]) {
+
 				// Change appended list to show letter
-				var modified = document.createElement("li");
+				var modified = document.getElementById(j);
 				modified.textContent = word[j];
-				parent.replaceChild(modified, parent.children[j]);
-				console.log("yep");
-			} else {
+
+				lettersGuessed++;
+			} else {				
+				hangs++;
 				console.log("nope");
 			}
+
 		}
+
+	hangMan();
+	// Detects if the player won
+	if (lettersGuessed == word.length) {
+		document.getElementById("numOfGuesses").innerHTML = "You won!";
+	}
 	console.log(input);
+	console.log(counter);
+	console.log(lettersGuessed);
+	console.log(hangs);
+//
+
+
+
+
+function hangMan() {
+
+
+
+
+	if (hangs == word.length) {
+		photo++;
+
+		//for (var k = 0; k <= photo; k++) {
+		//console.log("To the gallows!");
+		var head = document.querySelector("tr");
+		var visual = document.createElement("td");
+		visual.setAttribute("id", photo);              
+		var q = document.createTextNode("*"); 
+		visual.appendChild(q);
+		document.getElementById("hang").appendChild(visual);
+			//visual.insertBefore(visual, head);
+		
+		
+		//}
+	}
+	
+}
+//
 }
 
 
